158cbba2734177db53f7afe04638b87f50cdb9b6
Sean <craig.sean92@gmail.com>
2022-08-11 10:22:46 -0600

    commit tree 13f87cd780fcf9e533f133251e9bed2698a87089

---
+++ db/migrate/20220811160941_add_rides_and_taxis_to_passengers.rb
@@ -0,0 +1,4 @@
+class AddRidesAndTaxisToPassengers < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
---
+++ db/migrate/20220811161015_add_passenger_and_rides_to_taxis.rb
@@ -0,0 +1,4 @@
+class AddPassengerAndRidesToTaxis < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
